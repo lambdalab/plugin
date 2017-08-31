87f099ece61b983f05bd3358f9d5b55f31486e27
Mengwei Ding <mengwei.ding@gmail.com>
2017-08-31 01:00:07 -0700

    update to 0.6.3

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Insight.io GitLab Plugin
 
-**Notice**: *This repository is deprecated.*
-
 Follow the instructions below to get the latest release:
 
 1. Get the `plugin.js` file from new address: https://cdn.codatlas.cn/gitlab-plugin/plugin.js
--- insightio/plugin.js
+++ insightio/plugin.js
@@ -1,13 +1,13 @@
 // Please change the config
 lambdalabConfig = {
-  // Your Codatlas instance address
+  // Your Insight.io instance address
   url: "http://localhost:9000",
-  // When a symbol is not resolvable in your Codatlas instance, e.g. java.lang.String, it could search an external
+  // When a symbol is not resolvable in your Insight.io instance, e.g. java.lang.String, it could search an external
   // server for information, if it found the symbol in the external server, the plugin will show the type info, and
   // user would be able to directly navigate to the definition of the symbol in the external server.
   //
   // Set the value to "" if you don't want to symbol name to be sent out to other server.
-  externalUrl: "https://www.codatlas.com",
+  externalUrl: "https://insight.io",
   // If enablePlugin = false, the plugin will only affect user if they specifically append a ?enablePlugin=true query
   // parameter in their URL at first (they could disable it by append ?enablePlugin=false).
   // Otherwise, the plugin will be effecitve no matter what query parameter is set.
@@ -30,10 +30,26 @@ var loadBundle = function() {
 };
 loadBundle();
 
+var shouldReload = function(loc) {
+  var GITLAB_SOURCE_PAGE_URL_REGEXP = /^\/([^\/]+)\/([^\/]+)\/(blob)\/.*$/;
+  var GITLAB_MERGE_REQUEST_SOURCE_PAGE_URL_REGEXP = /^\/([^\/]+)\/([^\/]+)\/(merge_requests)\/([0-9]+)\/diffs$/;
+  var GITLAB_COMMIT_PAGE_URL_REGEXP = /^\/([^\/]+)\/([^\/]+)\/(commit)\/.*$/;
+
+  return GITLAB_SOURCE_PAGE_URL_REGEXP.exec(loc.pathname) ||
+	GITLAB_MERGE_REQUEST_SOURCE_PAGE_URL_REGEXP.exec(loc.pathname) ||
+	GITLAB_COMMIT_PAGE_URL_REGEXP.exec(loc.pathname);
+};
+
 window.onpopstate = function(event) {
-  window.location.reload();
+  const newLocation = event.currentTarget.location;
+  if (shouldReload(newLocation)) {
+    window.location.reload();
+  }
 };
 
 history.onpushstate = function(event) {
-  window.location.reload();
+  const newLocation = event.currentTarget.location;
+  if (shouldReload(newLocation)) {
+    window.location.reload();
+  }
 };
